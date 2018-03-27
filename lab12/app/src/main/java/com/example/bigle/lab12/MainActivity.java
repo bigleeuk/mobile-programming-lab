86babf0c2f43e39bb57fe51cd0426b839dac595c
Bigleeuk <bigleeuk@naver.com>
2018-03-27 15:37:33 +0900

    LAB1

---
+++ lab12/app/src/main/java/com/example/bigle/lab12/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.bigle.lab12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public EditText edit_name;
+
+    public Button btn_print;
+    public Button btn_clear;
+    public TextView view_print;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+    public void init(){
+        view_print = (TextView)findViewById(R.id.textview);
+        edit_name =(EditText)findViewById(R.id.edittext);
+        btn_clear=(Button) findViewById(R.id.btn_clear);
+        btn_print=(Button) findViewById(R.id.btn_print);
+        btn_print.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String text = "";
+                text = edit_name.getText().toString();
+                view_print.setText(text);
+            }
+        });
+        btn_clear.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                edit_name.setText("");
+                view_print.setText("");
+            }
+        });
+
+    }
